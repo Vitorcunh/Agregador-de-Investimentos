ee253b99dc47a12da13784ad455d72aed5e47ec9
Vitorcunh <vitorcunhasilva2580@gmail.com>
2024-08-19 21:18:46 -0300

    Primeiro commit

---
+++ src/main/java/tech/buildrun/agregadoinvestimentos/AgregadoinvestimentosApplication.java
@@ -0,0 +1,13 @@
+package tech.buildrun.agregadoinvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadoinvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadoinvestimentosApplication.class, args);
+	}
+
+}
