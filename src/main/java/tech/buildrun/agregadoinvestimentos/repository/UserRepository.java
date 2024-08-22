cb96ee96c5af48c0773a36246989f26bcb33a3b0
Vitorcunh <vitorcunhasilva2580@gmail.com>
2024-08-22 19:56:20 -0300

    Criamos o controller, Service e testamos requisição

---
+++ src/main/java/tech/buildrun/agregadoinvestimentos/repository/UserRepository.java
@@ -0,0 +1,2 @@
+package tech.buildrun.agregadoinvestimentos.repository;public interface UserRepository {
+}
---
+++ src/main/java/tech/buildrun/agregadoinvestimentos/service/UserService.java
@@ -0,0 +1,2 @@
+package tech.buildrun.agregadoinvestimentos.service;public class UserService {
+}
